f99a5d2953d03b771c0a9d966393fd713af15b9d
thejayadad <thejayadad@gmail.com>
2023-10-03 17:32:13 -0400

    update

--- myapp/components/Hero.js
+++ myapp/components/Hero.js
@@ -1,13 +1,12 @@
 'use client'
-
 import React, { useState, useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import ImageSlider from './ImageSlider';
 
 const Hero = () => {
   const [images] = useState([
-    'https://via.placeholder.com/400x300',
-    'https://via.placeholder.com/400x300',
+    'https://images.pexels.com/photos/2048548/pexels-photo-2048548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1306248/pexels-photo-1306248.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     'https://via.placeholder.com/400x300',
     'https://via.placeholder.com/400x300',
   ]);
@@ -15,22 +14,19 @@ const Hero = () => {
   const controls = useAnimation();
 
   useEffect(() => {
-    // Animate the hero section when it mounts
     controls.start({ opacity: 1, y: 0, transition: { duration: 1 } });
   }, [controls]);
 
   return (
     <section className='mx-auto max-w-screen-lg py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4'>
-      {/* Column 1: Image Slider */}
-      <motion.div
+      <div
         className='col-span-1 sm:col-span-2 md:col-span-1 bg-gray-200 p-4'
         initial={{ opacity: 0, y: -100 }}
         animate={controls}
       >
         <ImageSlider images={images} />
-      </motion.div>
+      </div>
 
-      {/* Column 2 */}
       <motion.div
         className='col-span-1 sm:col-span-1 md:col-span-1 bg-gray-300'
         initial={{ opacity: 0, y: -100 }}
@@ -38,13 +34,12 @@ const Hero = () => {
         transition={{ delay: 0.2 }}
       >
         <img
-          src='https://via.placeholder.com/400x300'
+          src='https://images.pexels.com/photos/1580267/pexels-photo-1580267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           alt='Image 2'
           className='w-full h-full'
         />
       </motion.div>
 
-      {/* Column 3 */}
       <motion.div
         className='col-span-1 sm:col-span-1 md:col-span-1 bg-gray-400'
         initial={{ opacity: 0, y: -100 }}
@@ -52,13 +47,12 @@ const Hero = () => {
         transition={{ delay: 0.4 }}
       >
         <img
-          src='https://via.placeholder.com/400x300'
+          src='https://images.pexels.com/photos/4462781/pexels-photo-4462781.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           alt='Image 3'
           className='w-full h-full'
         />
       </motion.div>
 
-      {/* Column 4 */}
       <motion.div
         className='col-span-1 sm:col-span-1 md:col-span-1 bg-gray-500'
         initial={{ opacity: 0, y: -100 }}
@@ -66,9 +60,9 @@ const Hero = () => {
         transition={{ delay: 0.6 }}
       >
         <img
-          src='https://via.placeholder.com/400x300'
+          src='https://images.pexels.com/photos/1522381/pexels-photo-1522381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           alt='Image 4'
-          className='w-full h-auto'
+          className='w-full h-full'
         />
       </motion.div>
     </section>
